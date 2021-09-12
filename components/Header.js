e782b84e10bb813d206cd502e68a402d74a2c182
Regoloale <72489833+Regoloale@users.noreply.github.com>
2021-09-12 18:09:39 +0200

    Header - left column

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,32 @@
+import Image from 'next/image';
+import { SearchIcon } from '@heroicons/react/solid';
+
 function Header() {
     return (
-        <div>
-            <h1>I am the Header</h1>
-        </div>
+        <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md py-5 px-5 md:px-10">
+
+            {/* Left */}
+            <div className="relative flex items-center h-10 cursor-pointer my-auto">
+                <Image 
+                    src="https://links.papareact.com/qd3"
+                    layout="fill"
+                    objectFit="contain"
+                    objectPosition="left"
+                />
+            </div>
+
+            {/* Middle - search */}
+            <div>
+                <input type="text" placeholder="Start your search" />
+                <SearchIcon />
+            </div>
+
+            {/* Right */}
+            <div>
+
+            </div>
+            
+        </header>
     )
 }
 
